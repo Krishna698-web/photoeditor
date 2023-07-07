be5cc443d9891189012faf23492c363590b4d901
Krishan <kmkrider1999@gmail.com>
2023-07-07 17:24:13 +0530

    Changed filter functionality

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 const input_file = document.getElementById("input_file");
-
-const image_div = document.getElementById("image_container");
 const image = document.getElementById("image");
-
-const image_downloader = document.getElementById("image_downloader");
 const download_image = document.getElementById("download_image");
 
+// Assign the loaded Image from input file
 input_file.addEventListener("change", () => {
   let image_file = input_file.files[0];
   image.src = URL.createObjectURL(image_file);
@@ -25,21 +22,19 @@ download_image.addEventListener("click", (e) => {
 });
 
 // Slider Ranging
-const range = document.getElementById("range");
-const inputRange = document.getElementById("inputRange");
-const span = document.getElementById("span");
-
 const opacity_range = document.getElementById("opacityControl");
 const grayscale_range = document.getElementById("grayscaleControl");
 const brightness_range = document.getElementById("brightnessControl");
 const contrast_range = document.getElementById("contrastControl");
 const saturate_range = document.getElementById("saturateControl");
+const tone_range = document.getElementById("toneControl");
 
 const setEffects = () => {
   image.style.filter = `opacity(${opacity_range.value}%) 
   brightness(${+brightness_range.value + 100}%) 
   grayscale(${grayscale_range.value}%) 
   saturate(${+saturate_range.value + 100}%)
+  sepia(${tone_range.value}%)
   contrast(${+contrast_range.value + 100}%)`;
 
   console.log(image.style.filter);
@@ -58,5 +53,8 @@ contrast_range.onchange = () => {
   setEffects();
 };
 saturate_range.onchange = () => {
+  setEffects();
+};
+tone_range.onchange = () => {
   setEffects();
 };
