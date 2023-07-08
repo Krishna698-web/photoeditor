cd3f70427a33370fadca01f3963729dfaf509679
Krishan <kmkrider1999@gmail.com>
2023-07-08 16:35:10 +0530

    Added canvas

--- script.js
+++ script.js
@@ -2,12 +2,59 @@ const input_file = document.getElementById("input_file");
 const image = document.getElementById("image");
 const download_image = document.getElementById("download_image");
 
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
 // Assign the loaded Image from input file
-input_file.addEventListener("change", () => {
-  let image_file = input_file.files[0];
-  image.src = URL.createObjectURL(image_file);
-  //   console.log(image.src);
+input_file.addEventListener("change", (e) => {
+  image.src = URL.createObjectURL(e.target.files[0]);
+
+  image.onload = () => {
+    // canvas.height = this.height;
+    // canvas.width = this.width;
+    canvas.crossOrigin = "anonymous";
+    setFilters();
+  };
+  console.log(image.src);
 });
+
+// Slider Ranging
+const opacity_range = document.getElementById("opacityControl");
+const grayscale_range = document.getElementById("grayscaleControl");
+const brightness_range = document.getElementById("brightnessControl");
+const contrast_range = document.getElementById("contrastControl");
+const saturate_range = document.getElementById("saturateControl");
+const tone_range = document.getElementById("toneControl");
+
+const setFilters = () => {
+  context.filter = `opacity(${opacity_range.value}%) 
+  brightness(${+brightness_range.value + 100}%) 
+  grayscale(${grayscale_range.value}%) 
+  saturate(${+saturate_range.value + 100}%)
+  sepia(${tone_range.value}%)
+  contrast(${+contrast_range.value + 100}%)`;
+
+  context.drawImage(image, 0, 0);
+};
+
+opacity_range.oninput = () => {
+  setFilters();
+};
+brightness_range.oninput = () => {
+  setFilters();
+};
+grayscale_range.oninput = () => {
+  setFilters();
+};
+contrast_range.oninput = () => {
+  setFilters();
+};
+saturate_range.oninput = () => {
+  setFilters();
+};
+tone_range.oninput = () => {
+  setFilters();
+};
 
 download_image.addEventListener("click", (e) => {
   e.preventDefault();
@@ -20,41 +67,3 @@ download_image.addEventListener("click", (e) => {
 
   console.log(download_image.href);
 });
-
-// Slider Ranging
-const opacity_range = document.getElementById("opacityControl");
-const grayscale_range = document.getElementById("grayscaleControl");
-const brightness_range = document.getElementById("brightnessControl");
-const contrast_range = document.getElementById("contrastControl");
-const saturate_range = document.getElementById("saturateControl");
-const tone_range = document.getElementById("toneControl");
-
-const setEffects = () => {
-  image.style.filter = `opacity(${opacity_range.value}%) 
-  brightness(${+brightness_range.value + 100}%) 
-  grayscale(${grayscale_range.value}%) 
-  saturate(${+saturate_range.value + 100}%)
-  sepia(${tone_range.value}%)
-  contrast(${+contrast_range.value + 100}%)`;
-
-  console.log(image.style.filter);
-};
-
-opacity_range.onchange = () => {
-  setEffects();
-};
-brightness_range.onchange = () => {
-  setEffects();
-};
-grayscale_range.onchange = () => {
-  setEffects();
-};
-contrast_range.onchange = () => {
-  setEffects();
-};
-saturate_range.onchange = () => {
-  setEffects();
-};
-tone_range.onchange = () => {
-  setEffects();
-};
