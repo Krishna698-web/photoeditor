878037769f0ea181e4bfcd26aa113176fc2e2d69
Krishan <kmkrider1999@gmail.com>
2023-07-09 23:05:38 +0530

    Added reset functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const input_file = document.getElementById("input_file");
 const image = document.getElementById("image");
 const download_image = document.getElementById("download_image");
+const reset_image = document.getElementById("reset_image");
 
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
@@ -24,6 +25,13 @@ const contrast_range = document.getElementById("contrastControl");
 const saturate_range = document.getElementById("saturateControl");
 const tone_range = document.getElementById("toneControl");
 
+const opacityVal = document.getElementById("opacity_val");
+const brightnessVal = document.getElementById("brightness_val");
+const grayscaleVal = document.getElementById("grayscale_val");
+const contrastVal = document.getElementById("contrast_val");
+const saturateVal = document.getElementById("saturate_val");
+const toneVal = document.getElementById("tone_val");
+
 const setFilters = () => {
   context.filter = `opacity(${opacity_range.value}%)
   brightness(${+brightness_range.value + 100}%)
@@ -35,22 +43,38 @@ const setFilters = () => {
   context.drawImage(image, 0, 0, 300, 550);
 };
 
+const setOpacity = () => {
+  context.filter = `opacity(${opacity_range.value / 10}%)`;
+  context.drawImage(image, 0, 0, 300, 550);
+};
+
 opacity_range.oninput = () => {
+  opacityVal.innerHTML = opacity_range.value;
   setFilters();
 };
+
 brightness_range.oninput = () => {
+  brightnessVal.innerHTML = brightness_range.value;
   setFilters();
 };
+
 grayscale_range.oninput = () => {
+  grayscaleVal.innerHTML = grayscale_range.value;
   setFilters();
 };
+
 contrast_range.oninput = () => {
+  contrastVal.innerHTML = contrast_range.value;
   setFilters();
 };
+
 saturate_range.oninput = () => {
+  saturateVal.innerHTML = saturate_range.value;
   setFilters();
 };
+
 tone_range.oninput = () => {
+  toneVal.innerHTML = tone_range.value;
   setFilters();
 };
 
@@ -63,4 +87,30 @@ download_image.addEventListener("click", (e) => {
     download_image.setAttribute("href", canvasData);
   }
   download_image.click();
+});
+
+reset_image.addEventListener("click", (e) => {
+  e.preventDefault();
+  opacity_range.value = 100 + "%";
+  brightness_range.value = 100 + "%";
+  grayscale_range.value = 0 + "%";
+  saturate_range.value = 0 + "%";
+  tone_range.value = 0 + "%";
+  contrast_range.value = 100 + "%";
+
+  saturateVal.innerHTML = saturate_range.value;
+  contrastVal.innerHTML = contrast_range.value;
+  grayscaleVal.innerHTML = grayscale_range.value;
+  brightnessVal.innerHTML = brightness_range.value;
+  opacityVal.innerHTML = opacity_range.value;
+  toneVal.innerHTML = tone_range.value;
+
+  context.filter = `opacity(${opacity_range.value}%)
+  brightness(${brightness_range.value}%)
+  grayscale(${grayscale_range.value}%)
+  saturate(${saturate_range.value}%)
+  sepia(${tone_range.value}%)
+  contrast(${contrast_range.value}%)`;
+
+  context.drawImage(image, 0, 0, 300, 550);
 });
